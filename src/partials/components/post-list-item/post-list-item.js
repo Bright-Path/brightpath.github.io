e8968064822345b5a7459b8e9e7f93404415f9e8
Ryan Dudek <ryandudek@gmail.com>
2021-08-22 14:29:04 -0400

    change bpbc prefix to bp

--- src/partials/components/post-list-item/post-list-item.js
+++ src/partials/components/post-list-item/post-list-item.js
@@ -1,6 +1,6 @@
 import $clamp from 'clamp-js';
 
-const module = document.querySelectorAll('.bpbc-post-link-title');
+const module = document.querySelectorAll('.bp-post-link-title');
 for (let index = 0; index < module.length; index++) {
     $clamp(module[index], { clamp: 3 });
 }
